01a447e6966bb2b4daf4c2cecb20962208da968b
KevinEmmanuelFrank <133403506+KevinEmmanuelFrank@users.noreply.github.com>
2023-05-18 20:28:25 -0300

    listo

--- src/main/java/org/company/Ejercicio1.java
+++ src/main/java/org/company/Ejercicio1.java
@@ -1,27 +1,6 @@
 package org.company;
-
-import java.util.Scanner;
-
 public class Ejercicio1 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Ingrese el primer valor: ");
-        double a = scanner.nextDouble();
-
-        System.out.println("Ingrese el segundo valor: ");
-        double b = scanner.nextDouble();
-
-        scanner.close();
-
-        double suma = a + b;
-        double resta = a - b;
-        double multiplicacion = a * b;
-        double division = a / b;
-
-        System.out.println("La suma: " + suma);
-        System.out.println("La resta: " + resta);
-        System.out.println("La multiplicación: " + multiplicacion);
-        System.out.println("La división: " + division);
+        System.out.println("Hola mundo");
     }
 }
--- src/main/java/org/company/Ejercicio2.java
+++ src/main/java/org/company/Ejercicio2.java
@@ -1,26 +1,15 @@
 package org.company;
 
 import java.util.Scanner;
+
 public class Ejercicio2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Ingrese el primer valor:");
-        double a = scanner.nextDouble();
-
-        System.out.println("Ingrese el segundo valor:");
-        double b = scanner.nextDouble();
+        System.out.println("Ingrese su nombre:");
+        String nombre = scanner.nextLine();
+        System.out.println("Hola, " + nombre + "!");
 
         scanner.close();
-
-        double suma = a + b;
-        double resta = a - b;
-        double multiplicacion = a * b;
-        double division = a / b;
-
-        System.out.println("La suma: " + suma);
-        System.out.println("La resta: " + resta);
-        System.out.println("La multiplicación: " + multiplicacion);
-        System.out.println("La división: " + division);
     }
 }
