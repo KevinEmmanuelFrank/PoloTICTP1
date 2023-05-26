c862cf9816e627522f292376ecec088957974103
KevinEmmanuelFrank <133403506+KevinEmmanuelFrank@users.noreply.github.com>
2023-05-26 18:29:40 -0300

    se Añade ej9

---
+++ src/main/java/Prueba.java
@@ -0,0 +1,14 @@
+public class Prueba {
+    private static void main(String[] args) {
+        int edadAlumnos[] = new int[3];
+        edadAlumnos[0] = 1;
+        edadAlumnos[1] = 2;
+        edadAlumnos[2] = 3;
+
+        for (int n = 0; n < 3; n++); {
+            int n = 0;
+            System.out.println(edadAlumnos[n]);
+            n++;
+        }
+    }
+}
