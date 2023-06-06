b98b585938451fd60f3e80ed0cacf12b9eac7ad4
KevinEmmanuelFrank <133403506+KevinEmmanuelFrank@users.noreply.github.com>
2023-06-06 10:59:58 -0300

    Creacion de Ventana
    Se creo una ventana con su Controlador

---
+++ src/main/java/controlador/Controlador.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador;
+
+import ventana.Ventana;
+
+/**
+ *
+ * @author AGUSTIN
+ */
+public class Controlador {
+    public static void main(String[] args) {
+        Ventana miVentana = new Ventana();
+        miVentana.setVisible(true);
+        }
+}
+
