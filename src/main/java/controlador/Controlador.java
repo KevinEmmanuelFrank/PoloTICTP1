4627826a008190ad356bd4408e44336478eda4a9
KevinEmmanuelFrank <133403506+KevinEmmanuelFrank@users.noreply.github.com>
2023-06-06 11:08:51 -0300

    Eliminacion
    Se elimino los comentarios

--- src/main/java/controlador/Controlador.java
+++ src/main/java/controlador/Controlador.java
@@ -1,15 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controlador;
-
 import ventana.Ventana;
 
-/**
- *
- * @author AGUSTIN
- */
 public class Controlador {
     public static void main(String[] args) {
         Ventana miVentana = new Ventana();
