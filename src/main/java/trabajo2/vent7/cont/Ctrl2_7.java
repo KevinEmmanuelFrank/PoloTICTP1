41d18048d6065eca81c97034af4cfe384aaf7152
KevinEmmanuelFrank <133403506+KevinEmmanuelFrank@users.noreply.github.com>
2023-08-16 11:17:01 -0300

    aaa
    aaa

---
+++ src/main/java/trabajo2/vent7/cont/Ctrl2_7.java
@@ -0,0 +1,11 @@
+
+package trabajo2.vent7.cont;
+
+
+
+public class Ctrl2_7 {
+    public static void main(String[] args) {
+        trabajo2.vent7.vent.Vent2_7 miVentana = new trabajo2.vent7.vent.Vent2_7();
+        miVentana.setVisible(true);
+        }
+}
